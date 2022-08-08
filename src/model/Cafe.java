aafdc09cf2636678957153a53d054a471b25bfb1
mini <gusals121234@gmail.com>
2022-08-08 14:44:47 +0900

    시준님이 만드신 cafe, cafemenu class file

--- src/model/Cafe.java
+++ src/model/Cafe.java
@@ -1,89 +1,96 @@
-/** 
-*PROJ:카페 데이터 관리*FILE:Cafe.java*DESC:카페 정보**@version 1.0**/
+/**
+ * PROJ : 카페 데이터 관리 
+ * FILE : Cafe.java
+ * DESC : 카페 정보 
+ * 
+ * @version 1.0
+**/
 package model;
-
-​
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
 public class Cafe {
-	/*** 카페 정보 종류 ***/
-	/** 카페 이름 **/
-	private String cafeName;
-	
-	/** 카페 위치 **/
+    /*** 카페 정보 종류 ***/
+    /** 카페 이름 **/
+    private String cafeName;
+    
+    /** 카페 위치 **/
     private String cafeLocation;
     
     /** 카페 오픈 시간 **/
-	private String cafeOpen;
-	
-	/** 카페 클로즈 시간 **/
-	private String cafeClose;
-	
-	/** 카페 전화번호 **/
-	private String cafeNo;
-	
-	/** 카페 스토어 매니저 **/
-	private String cafeSM;
-​
-	// constructor
-	public Cafe(String cafeName, String cafeLocation, String cafeOpen, String cafeClose, 
-			String cafeNo, String cafeSM) {
-		super();
-		this.cafeName = cafeName;
-		this.cafeLocation = cafeLocation;
-		this.cafeOpen = cafeOpen;
-		this.cafeClose = cafeClose;
-		this.cafeNo = cafeNo;
-		this.cafeSM = cafeSM;
-	}
-​
-	
-	// Getter & Setter
-	public String getCafeName() {
-		return cafeName;
-	}
-	public void setCafeName(String cafeName) {
-		this.cafeName = cafeName;
-	}
-	public String getCafeOpen() {
-		return cafeOpen;
-	}
-	public void setCafeOpen(String cafeOpen) {
-		this.cafeOpen = cafeOpen;
-	}
-	public String getCafeNo() {
-		return cafeNo;
-	}
-	public void setCafeNo(String cafeNo) {
-		this.cafeNo = cafeNo;
-	}
-	
-	// toString
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("카페명 : ");
-		builder.append(cafeName);
-		builder.append("\n");
-		    
-		builder.append("카페 위치 : ");
-		builder.append(cafeLocation);
-		builder.append("\n");
-		    
-		builder.append("카페 오픈 시간 : ");
-		builder.append(cafeOpen);
-		builder.append("\n");
-		    
-		builder.append("카페 클로즈 시간");
-		builder.append(cafeClose);
-		builder.append("\n");
-		    
-		builder.append("카페 전화번호");
-		builder.append(cafeNo);
-		builder.append("\n");
-		    
-		builder.append("카페 스토어 매니저");
-		builder.append(cafeSM);
-		builder.append("\n");
-		return builder.toString();
-	}
-​
+    private String cafeOpen;
+    
+    /** 카페 클로즈 시간 **/
+    private String cafeClose;
+    
+    /** 카페 전화번호 **/
+    private String cafeNo;
+    
+    /** 카페 스토어 매니저 **/
+    private String cafeSM;
+    
+    /** 카페 딜리버리 가능여부 **/
+    private char cafeDelivery;
+    // constructor
+    public Cafe(String cafeName, String cafeLocation, String cafeOpen, String cafeClose, 
+            String cafeNo, String cafeSM) {
+        super();
+        this.cafeName = cafeName;
+        this.cafeLocation = cafeLocation;
+        this.cafeOpen = cafeOpen;
+        this.cafeClose = cafeClose;
+        this.cafeNo = cafeNo;
+        this.cafeSM = cafeSM;
+    }
+    
+    // Getter & Setter
+    public String getCafeName() {
+        return cafeName;
+    }
+    public void setCafeName(String cafeName) {
+        this.cafeName = cafeName;
+    }
+    public String getCafeOpen() {
+        return cafeOpen;
+    }
+    public void setCafeOpen(String cafeOpen) {
+        this.cafeOpen = cafeOpen;
+    }
+    public String getCafeNo() {
+        return cafeNo;
+    }
+    public void setCafeNo(String cafeNo) {
+        this.cafeNo = cafeNo;
+    }
+    
+    // toString
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("카페명 : ");
+        builder.append(cafeName);
+        builder.append("\n");
+            
+        builder.append("카페 위치 : ");
+        builder.append(cafeLocation);
+        builder.append("\n");
+            
+        builder.append("카페 오픈 시간 : ");
+        builder.append(cafeOpen);
+        builder.append("\n");
+            
+        builder.append("카페 클로즈 시간");
+        builder.append(cafeClose);
+        builder.append("\n");
+            
+        builder.append("카페 전화번호");
+        builder.append(cafeNo);
+        builder.append("\n");
+            
+        builder.append("카페 스토어 매니저");
+        builder.append(cafeSM);
+        builder.append("\n");
+        return builder.toString();
+    }
 }
--- src/model/CafeMenu.java
+++ src/model/CafeMenu.java
@@ -1,14 +1,70 @@
+/**
+ * PROJ : 카페 데이터 관리
+ * FILE : CafeMenu.java
+ * DESC : 카페 메뉴 정보 
+ * 
+ * @version 1.0
+**/
 package model;
-
-import java.util.ArrayList;
-
 public class CafeMenu {
-	private ArrayList<String> desertList;
-
-	private ArrayList<String> Desert(String Deserts){
-		
-		this.desertList.add(Deserts);
-		return desertList;
-	};
-
+    /** 카페 메뉴 종류 **/
+    /** 메뉴 카테코리 **/
+    private String menuCategory;
+    
+    /** 메뉴 이름 **/
+    private String menuName;
+    
+    /** 메뉴 가격 **/
+    private int menuPrice;
+    
+    /** 메뉴 칼로리 **/
+    private String menuCalorie;
+    
+    
+    public CafeMenu() {}
+    public CafeMenu(String menuCategory, String menuName, int menuPrice, String menuCalorie) {
+        super();
+        this.menuCategory = menuCategory;
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.menuCalorie = menuCalorie;
+    }
+    
+    public String getMenuCategory() {
+        return menuCategory;
+    }
+    
+    public void setMenuCategory(String menuCategory) {
+        this.menuCategory = menuCategory;
+    }
+    
+    public String getMenuName() {
+        return menuName;
+    }
+   
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+    
+    public int getMenuPrice() {
+        return menuPrice;
+    }
+    
+    public void setMenuPrice(int menuPrice) {
+        this.menuPrice = menuPrice;
+    }
+    
+    public String getMenuCalorie() {
+        return menuCalorie;
+    }
+    
+    public void setMenuCalorie(String menuCalorie) {
+        this.menuCalorie = menuCalorie;
+    }
+    
+    
+    
+    
+    
+    
 }
