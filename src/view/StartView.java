4e976c2311878a688ca0005297cbda5b8d19b1bb
mini <gusals121234@gmail.com>
2022-08-08 14:43:55 +0900

    규리님이 만드신 startview 첨부

--- src/view/StartView.java
+++ src/view/StartView.java
@@ -1,12 +1,22 @@
 package view;
 
-import model.Clients;
+
+import controller.SearchModel;
+import controller.SelectModel;
+import model.CafeMenu;
 
 public class StartView {
-
-	public static void main(String[] args) {
-
-
+	public static void main(String [] args) {
+		
+		CafeMenu menu1 = new CafeMenu("Americano", 4000);
+		CafeMenu menu2 = new CafeMenu("Latte", 5000);
+		SearchModel instance = SearchModel.getInstance();
+		
+		SelectModel s = new SelectModel();
+		
+		instance.cafeMenuInsert(menu1);
+		instance.cafeMenuInsert(menu2);
+		
+		s.reqRes(0);
 	}
-
 }
